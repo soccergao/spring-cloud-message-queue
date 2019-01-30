1e48a300542c62bc35f721197254324e359aab37
soccergao <513346916@qq.com>
2019-01-30 22:08:50 +0800

    消息的手动确认

--- src/main/java/com/soccergao/mq/rabbit/config/RabbitConfig.java
+++ src/main/java/com/soccergao/mq/rabbit/config/RabbitConfig.java
@@ -28,4 +28,31 @@ public class RabbitConfig {
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
+//    /**
+//     * 确认消息（全局处理消息）(手动确认) 未验证
+//     * @param connectionFactory
+//     * @return
+//     */
+//    @Bean
+//    public SimpleMessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory){
+//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//        container.setConnectionFactory(connectionFactory);
+//        container.setQueueNames("consumer_queue");              // 监听的队列
+//        container.setAcknowledgeMode(AcknowledgeMode.MANUAL);        // 手动确认
+//        container.setMessageListener((ChannelAwareMessageListener) (message, channel) -> {      //消息处理
+//            System.out.println("====接收到消息=====");
+//            System.out.println(new String(message.getBody()));
+//            if(message.getMessageProperties().getHeaders().get("error") == null){   // 判断消息头是否有error属性
+//                channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+//                System.out.println("消息已经确认");
+//            }else {
+//                //channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,false);
+//                channel.basicReject(message.getMessageProperties().getDeliveryTag(),false);
+//                System.out.println("消息拒绝");
+//            }
+//
+//        });
+//        return container;
+//    }
 }
--- src/main/java/com/soccergao/mq/rabbit/listener/Listener.java
+++ src/main/java/com/soccergao/mq/rabbit/listener/Listener.java
@@ -22,9 +22,39 @@ public class Listener {
     @RabbitListener(queues = Constant.DIRECT_QUEUE)
     public void directListener(User userResponse) {
         System.out.println(userResponse.getUsername());
-//        System.out.println(userResponse);
         throw new NullPointerException();
     }
+
+//    /**
+//     *
+//     * @param userResponse
+//     * @param channel
+//     * @param map 消息头的内容, 获取DELIVERY_TAG也可以直接使用@Header(AmqpHeaders.DELIVERY_TAG) long tag
+//     * @throws IOException
+//     */
+//    @RabbitListener(queues = Constant.DIRECT_QUEUE)
+//    public void directListener(User userResponse, Channel channel, @Headers Map<String, Object> map/*, @Header(AmqpHeaders.DELIVERY_TAG) long tag*/) throws IOException {
+//        System.out.println(userResponse.getUsername());
+//        /**
+//         * ack
+//         *
+//         * multiple：为了减少网络流量，手动确认可以被批处理，当该参数为 true 时，则可以一次性确认 delivery_tag 小于等于传入值的所有消息
+//         */
+//        channel.basicAck((Long) map.get(AmqpHeaders.DELIVERY_TAG), false);
+//        /**
+//         * nack
+//         *
+//         * multiple：为了减少网络流量，手动确认可以被批处理，当该参数为 true 时，则可以一次性确认 delivery_tag 小于等于传入值的所有消息
+//         * requeue：是否丢弃, true为不丢弃重回队列, false为丢弃或进入死信队列
+//         */
+//        channel.basicNack((Long) map.get(AmqpHeaders.DELIVERY_TAG), false, true);
+//        /**
+//         * reject
+//         *
+//         * requeue：是否丢弃, true为不丢弃重回队列, false为丢弃或进入死信队列
+//         */
+//        channel.basicReject((Long) map.get(AmqpHeaders.DELIVERY_TAG), true);
+//    }
 
     @RabbitListener(queues = Constant.TOPIC_QUEUE)
     public void topicListener(String message) {
